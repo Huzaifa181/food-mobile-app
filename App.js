d55f060b41428eaa5ab25d1711d8d98a80a341db
Huzaifa181 <huzaifa.ahmed181@gmail.com>
2021-07-23 03:46:33 +0500

    Basic Structure

---
+++ App.js
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  TextInput,
+  useColorScheme,
+  View,
+  FlatList,
+  Text,
+  Button,
+} from 'react-native';
+import {
+  Colors,
+  DebugInstructions,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
+const App = () => {
+  return <View padding={30}></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
